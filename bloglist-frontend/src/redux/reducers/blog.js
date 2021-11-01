ad8bc1d4b01c031c2fd87be0084f420cfc6733c2
Pasang Nuru Sherpa <79486506+solu22@users.noreply.github.com>
2021-11-01 02:56:10 +0200

    7.9 -7.21

--- bloglist-frontend/src/redux/reducers/blog.js
+++ bloglist-frontend/src/redux/reducers/blog.js
@@ -73,7 +73,7 @@ export const deleteBlog = (id) => {
 }
 
 export const addComment = (blog, comment) => {
-  return async dispatch => {
+  return async dispatch.comment => {
     const data = await blogService.update({
       ...blog, comments: blog.comments.concat([comment])
     })
